4f5a9f1fe47e801c362847784e94b68327c3f143
steven-patterson17 <turkpatterson15@gmail.com>
2026-02-25 16:02:10 -0500

    2/26/2026

--- Assets/BallReturnMetricsProvider.cs
+++ Assets/BallReturnMetricsProvider.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class BallReturnMetricsProvider : MonoBehaviour
@@ -5,6 +6,9 @@ public class BallReturnMetricsProvider : MonoBehaviour
     public float ReturnAngle { get; private set; }
     public float ReturnSpeed { get; private set; }
     public float ReturnSpin { get; private set; }
+
+    // Raised when the ball is returned by a paddle. Args: speed, spin, angle, distance
+    public static event Action<float, float, float, float> OnBallReturn;
 
     private Rigidbody rb;
 
@@ -31,6 +35,11 @@ public class BallReturnMetricsProvider : MonoBehaviour
 
             // Notify UI
             MetricsBoardUI.Instance.SetReturnMetrics(ReturnAngle, ReturnSpeed, ReturnSpin);
+
+            // Notify subscribers (e.g., session aggregator). Provide distance if available.
+            var distanceProv = GetComponent<BallDistanceProvider>();
+            float distance = distanceProv != null ? distanceProv.Distance : 0f;
+            OnBallReturn?.Invoke(ReturnSpeed, ReturnSpin, ReturnAngle, distance);
         }
     }
 }
