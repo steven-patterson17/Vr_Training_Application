02769ce937a1cc6e4a67d32fd40875168abab3bd
CLeimkuhler <connorleimkuhler@gmail.com>
2026-04-22 12:54:21 -0400

    Update Haptic.cs
    Remove "namespace VRTraining" if necessary, require for fixing reliability issues on SonarQube

--- Assets/Haptic.cs
+++ Assets/Haptic.cs
@@ -6,45 +6,48 @@ using UnityEngine.XR;
 /// Controls the feedback given to the controllers
 /// when user hits the ball
 /// </summary>
-public class Haptic : MonoBehaviour
+namespace VRTraining
 {
-// public XRNode controllerNode = XRNode.RightHand; // set LeftHand or RightHand in Inspector
-
-
-
-    [Range(0f, 1f)] public float amplitude = 0.7f;
-    public float duration = 0.08f;
-
-    private InputDevice device;
-    private XRNode controllerNode;
-
-    void Start()
+    public class Haptic : MonoBehaviour
     {
-        //controllerNode = SessionMetricsManager.IsLeftHanded ? XRNode.LeftHand : XRNode.RightHand;
-        //device = InputDevices.GetDeviceAtXRNode(controllerNode);
-        
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("PingPongBall"))
+    // public XRNode controllerNode = XRNode.RightHand; // set LeftHand or RightHand in Inspector
+    
+    
+    
+        [Range(0f, 1f)] public float amplitude = 0.7f;
+        public float duration = 0.08f;
+    
+        private InputDevice device;
+        private XRNode controllerNode;
+    
+        void Start()
         {
-            SendHaptic();
+            //controllerNode = SessionMetricsManager.IsLeftHanded ? XRNode.LeftHand : XRNode.RightHand;
+            //device = InputDevices.GetDeviceAtXRNode(controllerNode);
+            
         }
-    }
-
-    void SendHaptic()
-    {
-        controllerNode = SessionMetricsManager.IsLeftHanded ? XRNode.LeftHand : XRNode.RightHand;
-        device = InputDevices.GetDeviceAtXRNode(controllerNode);
-        if (!device.isValid)
-            device = InputDevices.GetDeviceAtXRNode(controllerNode);
-
-
-        if (device.TryGetHapticCapabilities(out HapticCapabilities capabilities) &&
-            capabilities.supportsImpulse)
+    
+        void OnCollisionEnter(Collision collision)
         {
-            device.SendHapticImpulse(0, amplitude, duration);
+            if (collision.gameObject.CompareTag("PingPongBall"))
+            {
+                SendHaptic();
+            }
+        }
+    
+        void SendHaptic()
+        {
+            controllerNode = SessionMetricsManager.IsLeftHanded ? XRNode.LeftHand : XRNode.RightHand;
+            device = InputDevices.GetDeviceAtXRNode(controllerNode);
+            if (!device.isValid)
+                device = InputDevices.GetDeviceAtXRNode(controllerNode);
+    
+    
+            if (device.TryGetHapticCapabilities(out HapticCapabilities capabilities) &&
+                capabilities.supportsImpulse)
+            {
+                device.SendHapticImpulse(0, amplitude, duration);
+            }
         }
     }
 }
