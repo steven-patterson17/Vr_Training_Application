a151474506ad728669dbd9cc2d972baa02b80435
CLeimkuhler <connorleimkuhler@gmail.com>
2026-04-22 12:25:00 -0400

    Update BallDestroy.cs
    Remove namespace VRTraining if necessary, require for fixing reliability issues on SonarQube

--- Assets/BallDestroy.cs
+++ Assets/BallDestroy.cs
@@ -7,19 +7,22 @@ using UnityEngine;
 /// <summary>
 /// Instantly destroys the ball if the ball is out of play
 /// </summary>
-public class DestroyOnHit : MonoBehaviour
+namespace VRTraining
 {
-    /// <summary>
-    /// Called when the object collides with another collider.
-    /// If the collision is with the floor or a wall, the object is destroyed.
-    /// </summary>
-    /// <param name="collision">Collision data containing information about the impact.</param>
-    private void OnCollisionEnter(Collision collision)
+    public class DestroyOnHit : MonoBehaviour
     {
-        if (collision.collider.CompareTag("Floor") ||
-            collision.collider.CompareTag("Wall"))
+        /// <summary>
+        /// Called when the object collides with another collider.
+        /// If the collision is with the floor or a wall, the object is destroyed.
+        /// </summary>
+        /// <param name="collision">Collision data containing information about the impact.</param>
+        private void OnCollisionEnter(Collision collision)
         {
-            Destroy(gameObject);
+            if (collision.collider.CompareTag("Floor") ||
+                collision.collider.CompareTag("Wall"))
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
