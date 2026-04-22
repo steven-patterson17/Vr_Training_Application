07d6a4b7d16ed0115ea285d1c2a6a9f2a20257d8
CLeimkuhler <connorleimkuhler@gmail.com>
2026-04-22 12:26:27 -0400

    Update BallDistanceProvider.cs
    Remove "namespace VRTraining" if necessary, require for fixing reliability issues on SonarQube

--- Assets/BallDistanceProvider.cs
+++ Assets/BallDistanceProvider.cs
@@ -4,29 +4,32 @@ using UnityEngine;
 /// Provides the distance the ball travels to
 /// he metric board
 /// </summary>
-public class BallDistanceProvider : MonoBehaviour
+namespace VRTraining
 {
-    /// <summary>
-    /// The starting position of the ball when it is initialized.
-    /// </summary>
-    private Vector3 startPos;
-
-    /// <summary>
-    /// Gets the total distance traveled by the ball from its starting position.
-    /// </summary>
-    public float Distance { get; private set; }
-
-    public void Start()
+    public class BallDistanceProvider : MonoBehaviour
     {
-        startPos = transform.position;
-    }
-
-    /// <summary>
-    /// Updates the distance value each frame based on the current position
-    /// relative to the starting position.
-    /// </summary>
-    void Update()
-    {
-        Distance = Vector3.Distance(startPos, transform.position);
+        /// <summary>
+        /// The starting position of the ball when it is initialized.
+        /// </summary>
+        private Vector3 startPos;
+    
+        /// <summary>
+        /// Gets the total distance traveled by the ball from its starting position.
+        /// </summary>
+        public float Distance { get; private set; }
+    
+        public void Start()
+        {
+            startPos = transform.position;
+        }
+    
+        /// <summary>
+        /// Updates the distance value each frame based on the current position
+        /// relative to the starting position.
+        /// </summary>
+        void Update()
+        {
+            Distance = Vector3.Distance(startPos, transform.position);
+        }
     }
 }
